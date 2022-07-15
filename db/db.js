68addc8da83cefedbff3ea5484cc1095b01d9e9a
Belousov Vitaliy <www.belousov.vitaliy@gmail.com>
2022-07-15 15:16:30 +0300

    fix

--- db/db.js
+++ db/db.js
@@ -1,12 +1,24 @@
 import Pool from 'pg-pool'
 import 'dotenv/config'
 
-const pool = new Pool({
+const devConfig = {
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-})
+  host: process.env.DB_HOST,
+}
+
+const productionConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
+}
+
+const pool = new Pool(
+  process.env.NODE_ENV === 'production' ? productionConfig : devConfig
+)
 
 export default pool
